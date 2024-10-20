1a801ee7c69bc70b68051a50a46eac21ab140ce7
Mohamed Eid <mohamedeid5415@gmail.com>
2024-10-20 07:11:16 +0300

    Add files via upload

---
+++ data_types.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+char a;
+int b;
+long c;
+float d;
+double e;
+
+cin>> a >> b >> c >> d >> e;
+
+cout<< a " "<< b " "<<c " "<<d " "<< e " ";
+
+
+
+
+
+
+}
